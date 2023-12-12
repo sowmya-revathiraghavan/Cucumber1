f51c14c06a496f47e87b5ab55a998e56bfa28054
sowmya-revathiraghavan <sowmya.revathiraghavan@gmail.com>
2023-12-12 16:54:44 +0530

    hello

---
+++ normal.html/report.js
@@ -0,0 +1,367 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("AutomationPractice.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Functionalities for Regression Suite - 1",
+  "description": "",
+  "id": "functionalities-for-regression-suite---1",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 1617500,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 143100,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Verify the customer or user is able to Login the Application",
+  "description": "",
+  "id": "functionalities-for-regression-suite---1;verify-the-customer-or-user-is-able-to-login-the-application",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@RegressionTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "User launch the AutomationPractice application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "User Click on SignIn Button in the header",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "User Enter email Id \"abcdef@gmail.com\" in SignIn page",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "User Enter Password \"12345\" in SignIn page",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "User Click SignIn button in SignIn page",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "User Verify the UserName displayed in the header",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "AutomationStepDefinition.user_launch_the_AutomationPractice_application()"
+});
+formatter.result({
+  "duration": 15299304400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomationStepDefinition.user_Click_on_SignIn_Button_in_the_header()"
+});
+formatter.result({
+  "duration": 1956559401,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "abcdef@gmail.com",
+      "offset": 21
+    }
+  ],
+  "location": "AutomationStepDefinition.user_Enter_email_Id_in_SignIn_page(String)"
+});
+formatter.result({
+  "duration": 1413198901,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "12345",
+      "offset": 21
+    }
+  ],
+  "location": "AutomationStepDefinition.user_Enter_Password_in_SignIn_page(String)"
+});
+formatter.result({
+  "duration": 171958400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomationStepDefinition.user_Click_SignIn_button_in_SignIn_page()"
+});
+formatter.result({
+  "duration": 6042887000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomationStepDefinition.user_Verify_the_UserName_displayed_in_the_header()"
+});
+formatter.result({
+  "duration": 33600,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 182401,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 93100,
+  "status": "passed"
+});
+formatter.uri("AutomationPractice2.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Functionalities for Regression Suite - 2",
+  "description": "",
+  "id": "functionalities-for-regression-suite---2",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 78100,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 72600,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Verify the customer or user is able to Login the Application",
+  "description": "",
+  "id": "functionalities-for-regression-suite---2;verify-the-customer-or-user-is-able-to-login-the-application",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@RegressionTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "User launch the AutomationPractice application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "User Click on SignIn Button in the header",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "User Enter email Id \"abcdef@gmail.com\" in SignIn page",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "User Enter Password \"12345\" in SignIn page",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "User Click SignIn button in SignIn page",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "User Verify the UserName displayed in the header",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "AutomationStepDefinition.user_launch_the_AutomationPractice_application()"
+});
+formatter.result({
+  "duration": 2688663700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomationStepDefinition.user_Click_on_SignIn_Button_in_the_header()"
+});
+formatter.result({
+  "duration": 1464138900,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "abcdef@gmail.com",
+      "offset": 21
+    }
+  ],
+  "location": "AutomationStepDefinition.user_Enter_email_Id_in_SignIn_page(String)"
+});
+formatter.result({
+  "duration": 1818469200,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "12345",
+      "offset": 21
+    }
+  ],
+  "location": "AutomationStepDefinition.user_Enter_Password_in_SignIn_page(String)"
+});
+formatter.result({
+  "duration": 230894000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomationStepDefinition.user_Click_SignIn_button_in_SignIn_page()"
+});
+formatter.result({
+  "duration": 2134312900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomationStepDefinition.user_Verify_the_UserName_displayed_in_the_header()"
+});
+formatter.result({
+  "duration": 55300,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 65901,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 44300,
+  "status": "passed"
+});
+formatter.uri("AutomationPractice3.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Functionalities for Regression Suite - 3",
+  "description": "",
+  "id": "functionalities-for-regression-suite---3",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 94000,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 71900,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Verify the customer or user is able to Login the Application",
+  "description": "",
+  "id": "functionalities-for-regression-suite---3;verify-the-customer-or-user-is-able-to-login-the-application",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@RegressionTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "User launch the AutomationPractice application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "User Click on SignIn Button in the header",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "User Enter email Id \"abcdef@gmail.com\" in SignIn page",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "User Enter Password \"12345\" in SignIn page",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "User Click SignIn button in SignIn page",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "User Verify the UserName displayed in the header",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "AutomationStepDefinition.user_launch_the_AutomationPractice_application()"
+});
+formatter.result({
+  "duration": 2969388300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomationStepDefinition.user_Click_on_SignIn_Button_in_the_header()"
+});
+formatter.result({
+  "duration": 1782379900,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "abcdef@gmail.com",
+      "offset": 21
+    }
+  ],
+  "location": "AutomationStepDefinition.user_Enter_email_Id_in_SignIn_page(String)"
+});
+formatter.result({
+  "duration": 535453500,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "12345",
+      "offset": 21
+    }
+  ],
+  "location": "AutomationStepDefinition.user_Enter_Password_in_SignIn_page(String)"
+});
+formatter.result({
+  "duration": 208711400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomationStepDefinition.user_Click_SignIn_button_in_SignIn_page()"
+});
+formatter.result({
+  "duration": 1878531500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomationStepDefinition.user_Verify_the_UserName_displayed_in_the_header()"
+});
+formatter.result({
+  "duration": 22999,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 48101,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 54301,
+  "status": "passed"
+});
+});
